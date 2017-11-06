255b2f6d1c0e6b9154037a1a5d65f7f1887c301b
Andreas Schneider <an.schneider@aon.at>
2017-11-06 03:16:39 +0100

    code cleanup, added animations, fixed maps zoom level

--- app/src/main/java/net/sytes/schneider/mobilechill/MainActivity.java
+++ app/src/main/java/net/sytes/schneider/mobilechill/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,25 +19,54 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private SupportMapFragment mapFragment;
-
 
     private TextView mTextMessage;
+    private FrameLayout dashboard;
+    private FrameLayout notifications;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        //Navigation
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_activity_main);
+                    if(notifications.getTranslationY() == 0 ){
+                        notifications.animate()
+                                .translationY(-notifications.getHeight());
+                    }
+                    if(dashboard.getTranslationY() == 0 ){
+                        dashboard.animate()
+                                .translationY(-dashboard.getHeight());
+                    }
                     return true;
+
+
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    if(dashboard.getTranslationY() == -dashboard.getHeight() ) {
+                        dashboard.animate()
+                                .translationY(0);
+                    }
+                    if(notifications.getTranslationY() == 0 ){
+                        notifications.animate()
+                                .translationY(-notifications.getHeight());
+                    }
+
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    if(notifications.getTranslationY() == -notifications.getHeight() ){
+                        notifications.animate()
+                                .translationY(0);
+                    }
+                    if(dashboard.getTranslationY() == 0 ){
+                        dashboard.animate()
+                                .translationY(-dashboard.getHeight());
+                    }
                     return true;
             }
             return false;
@@ -52,43 +82,25 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        dashboard = (FrameLayout) findViewById(R.id.dashboard);
+        notifications = (FrameLayout) findViewById(R.id.notifications);
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        if( mMap == null){
+        if( mMap == null) {
             try {
                 SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.map);
                 mapFragment.getMapAsync(this);
             } catch (Exception e) {
-                Log.e("YOLONAUTEN", "onCreateView", e);
+                Log.e("MainActivity", "Failed to create map!", e);
                 throw e;
             }
         }
-
-/*
-        if (mapFragment != null) {
-            mapFragment = SupportMapFragment.newInstance();
-            mapFragment.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    mMap = googleMap;
-
-                    // Add a marker in Sydney and move the camera
-                    LatLng sydney = new LatLng(-34, 151);
-                    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-                }
-            });
-        }
-
-        //R.id.map is a FrameLayout, not a Fragment
-        getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
-
-        */
 
     }
 
@@ -106,16 +118,18 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         try {
-
             // Add a marker in Sydney and move the camera
-            LatLng sydney = new LatLng(-34, 151);
-            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            LatLng graz = new LatLng(47.074458, 15.438041);                 //	Latitude, Longitude in degrees.
+            mMap.addMarker(new MarkerOptions().position(graz).title("Marker in Graz"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(graz));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(graz, 12.0f));
 
         } catch (Exception e) {
-            Log.e("YOLONAUTEN", "onCreateView", e);
+            Log.e("MainActivity", "Failed to access map!", e);
             throw e;
         }
+        dashboard.animate().translationY(-dashboard.getHeight());
+        notifications.animate().translationY(-notifications.getHeight());
     }
 
 
